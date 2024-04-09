1fa5ba90b5448f52b864e41f45be387690bb345c
Robert Crummett <rncrummett5@gmail.com>
2024-04-09 14:43:02 -0600

    Removing netcdf files

--- preprocess.py
+++ preprocess.py
@@ -1,71 +1,111 @@
-''' Parse raw GXF data file, Save as Xarray
+""" Parse raw GXF data file, Save as Xarray
 
 Raw data retrieved from:
 https://pubs.usgs.gov/of/2001/ofr-01-0081/html/arizona.html
-'''
+
+Author: R Nate Crummett
+"""
+
 from pathlib import Path
 
 import numpy as np
+from pyproj import CRS
 import xarray as xr
 
 ##############################################
-# Da#ta paths
+# Raw data path
 data_path = Path("az1000ag_gxf")
-output_path = Path(f"{data_path.stem}.nc")
 
-# Meta data
-xorigin = -269_500
-yorigin = 35_000
-rotation = 0
-ptsep = 500
-rwsep = 500
-units = "meters"
-mapproj = "NAD27 / az_lambert\nNAD27,6378206.4,0.082271854,0\nLambert Conic Conformal (2SP),33,45,31,-112,0,0"
-xsize = 1_134
-ysize = 1_285
-null_value = -1e32
+# Output NETCDF path
+lcc_nad27_path = Path(f"az1000_lcc_nad27.nc")
+
+##############################################
+# Projection data extracted manually from raw data file
+
+# Provided map projection data
+projection = "lcc"  # lambert conformal conic 2SP
+datum = "NAD27"  # NAD27 datum
+lat_1, lat_2 = 33, 45  # first and second standard parallels
+lat_0 = 31  # latitude of natural origin
+lon_0 = -112  # centeral meridian
+semimajor = 6378206.4  # semi major axis
+e = 0.082271854  # eccentricity
+units = "m"  # units of meters
+false_easting = 0  # easting at false origin meters
+false_northing = 0  # northing at false origin meters
+scale = 1  # ellipsoid scale factor
+
+# Provided grid meta data
+xsize = 1_134  # grid X size
+ysize = 1_285  # grid Y size
+xorigin = -269_500  # grid X origin meters
+yorigin = 35_000  # grid Y origin meters
+rotation = 0  # grid rotation degrees
+ptsep = 500  # column seperation meters
+rwsep = 500  # grid seperation meters
+null_value = -1e32  # grid null value
+
+# Compute flatness from provided eccentricity
+flatness = 1 - np.sqrt(1 - e**2)  # flatness
+
+# Assemble proj information into CRS object
+lcc_nad27_dict = dict(
+    proj=projection,
+    lat_1=lat_1,
+    lat_2=lat_2,
+    lat_0=lat_0,
+    lon_0=lon_0,
+    datum=datum,
+    a=semimajor,
+    f=flatness,
+    x_0=false_easting,
+    y_0=false_northing,
+    k_0=scale,
+    units=units,
+)
+
+crs_lcc_nad27 = CRS.from_dict(lcc_nad27_dict)
 
 #############################################
+# Parse raw data
+
 # Preallocate space
-data = np.empty(xsize*ysize, dtype = float)
+data = np.empty(xsize * ysize, dtype=float)
 
 # Parse raw file
 with open(data_path, "r") as file:
-    # Discard header lines....
+    # Discard header lines...
     for line in file:
         if "#GRID" in line:
             break
     # Translate text to float32 array
     index = 0
     for line in file:
-        elements = np.array(line.split(), dtype = float)
-        elements[elements == null_value] = np.nan # Null values are set to nan
+        elements = np.array(line.split(), dtype=float)  # Does not assume line size
+        elements[elements == null_value] = np.nan  # Null values are set to nan
         span = len(elements)
-        data[index:index+span] = elements
+        data[index : index + span] = elements
         index += span
 
 # Reshape data array
 # Easting (y) should be first coordinate
-data = data.reshape((ysize, xsize)) 
+data = data.reshape((ysize, xsize))
 
 # Construct positions
-x    = np.arange(0.0, xsize * rwsep, rwsep) + xorigin
-y    = np.arange(0.0, ysize * ptsep, ptsep) + yorigin
+x = np.arange(0.0, xsize * rwsep, rwsep) + xorigin
+y = np.arange(0.0, ysize * ptsep, ptsep) + yorigin
 
 # Format all as Xarray
 ds = xr.Dataset(
-        data_vars = dict(tfa = (["yc", "xc"], data)),
-        coords = dict(x = (["xc"], x),
-                      y = (["yc"], y)),
-        attrs = dict(
-            description = "Arizona Aeromagnetic Anomaly Map, Continued 1000 Feet Above Ground",
-            dx = rwsep,
-            dy = ptsep,
-            units = units,
-            mapproj = mapproj, 
-            xorigin = xorigin,
-            yorigin = yorigin,
-            rotation = rotation))
+    data_vars=dict(tfa=(["yc", "xc"], data)),
+    coords=dict(x=(["xc"], x), y=(["yc"], y)),
+    attrs=dict(
+        description="Arizona Aeromagnetic Anomaly Map, Continued 1000 Feet Above Ground",
+        dx=rwsep,
+        dy=ptsep,
+        proj_wkt=crs_lcc_nad27.to_wkt(),
+    ),
+)
 
 # Write data to disk
-ds.to_netcdf(output_path)
+ds.to_netcdf(lcc_nad27_path)
---
+++ reproject.py
@@ -0,0 +1,52 @@
+""" Reproject NETCDF file
+
+Author: R Nate Crummett
+"""
+
+from pathlib import Path
+
+import numpy as np
+from pyproj import CRS, Transformer
+import xarray as xr
+
+###################################################
+# Input file
+lcc_nad27_path = Path("az1000_lcc_nad27.nc")
+
+# Target CRS
+crs_target = CRS.from_epsg(4326)  # WGS84
+
+# Output file
+target_path = Path("az1000_wgs84.nc")
+
+###################################################
+# Import data
+dataset_lcc_nad27 = xr.load_dataset(lcc_nad27_path)
+
+# Extract CSR from data
+crs_lcc_nad27 = CRS.from_wkt(dataset_lcc_nad27.proj_wkt)
+
+# Grid positional data
+x = dataset_lcc_nad27.x.values
+y = dataset_lcc_nad27.y.values
+X, Y = np.meshgrid(x, y)
+
+# Create transformation between CRS's
+trans = Transformer.from_crs(crs_lcc_nad27, crs_target, always_xy=True)
+longitude, latitude = trans.transform(X, Y)
+
+####################################################
+# Save output
+
+# Format all as Xarray
+ds = xr.Dataset(
+    data_vars=dict(tfa=(["yc", "xc"], dataset_lcc_nad27.tfa.values)),
+    coords=dict(lon=(["yc", "xc"], longitude), lat=(["yc", "xc"], latitude)),
+    attrs=dict(
+        description="Arizona Aeromagnetic Anomaly Map, Continued 1000 Feet Above Ground",
+        proj_wkt=crs_target.to_wkt(),
+    ),
+)
+
+# Write reprojected data to NETCDF
+ds.to_netcdf(target_path)
